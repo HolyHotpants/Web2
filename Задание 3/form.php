8f25f350638568264fc390570c8356e3c6615ca0
HolyHotpants <91026415+HolyHotpants@users.noreply.github.com>
2022-03-27 19:04:40 +0300

    Update form.php

--- Задание 3/form.php
+++ Задание 3/form.php
@@ -13,14 +13,6 @@
             $Email=$_POST["email"];
             $DateBirth=$_POST["date"];
             $Sex=$_POST["sex"];
-            /*if($_POST["sex_M"]==1)
-                $Sex='M';
-            else
-                $Sex='W';
-            if($_POST["count_limbs_l2"]==1)
-                $count_limbs='<2';
-            else
-                $count_limbs='>2';*/
             $Count_limbs=$_POST["count_limbs"];
             $Abilitys="";
             $Superpowers=$_POST["superpowers"];
@@ -30,13 +22,13 @@
             $Biography=$_POST["biography"];
             $Insert_form=$conn->prepare("INSERT INTO Ex3 (Fio, Email, Date_birth, Sex, Count_limbs, Abilitys, Biography) 
                                         VALUES (':name', ':email', :date, ':sex', ':count_limbs', ':superpowers', ':biography')");
-            $Insert_form->bindValue(':name',$Name);
-            $Insert_form->bindValue(':email',$Email);
-            $Insert_form->bindValue(':date',$DateBirth);
-            $Insert_form->bindValue(':sex',$Sex);
-            $Insert_form->bindValue(':count_limbs',$Count_limbs);
-            $Insert_form->bindValue(':superpowers',$Abilitys);
-            $Insert_form->bindValue(':biography',$Biography);
+            $Insert_form->bindParam(':name',$Name);
+            $Insert_form->bindParam(':email',$Email);
+            $Insert_form->bindParam(':date',$DateBirth);
+            $Insert_form->bindParam(':sex',$Sex);
+            $Insert_form->bindParam(':count_limbs',$Count_limbs);
+            $Insert_form->bindParam(':superpowers',$Abilitys);
+            $Insert_form->bindParam(':biography',$Biography);
             $Insert_pform=$Insert_form->execute();
 
         }
