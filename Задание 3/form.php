0cebd7dbb648ca02a26ba7cd0ab475b6b10a2264
HolyHotpants <91026415+HolyHotpants@users.noreply.github.com>
2022-03-28 20:17:16 +0300

    Update form.php

--- Задание 3/form.php
+++ Задание 3/form.php
@@ -1,7 +1,17 @@
 <?php
     header('Content-Type: text/html; charset=UTF-8');
 
-    $conn= new PDO('mysql:host=localhost;dbname=u47542', "u47542", "7615565", array(PDO::ATTR_PERSISTENT => true));
+    if ($_SERVER['REQUEST_METHOD'] == 'GET')	
+    {
+        if (!empty($_GET['save']))
+        {
+            print('Спасибо, результаты сохранены.');
+        }
+        include('form.php');
+        exit();
+    }
+
+    
     if(isset($_POST["name"]) && isset($_POST["email"]) && 
         isset($_POST["date"]) && (isset($_POST["sex_M"])||isset($_POST["sex_W"]))&&
         (isset($_POST["count_limbs_l2"])||isset($_POST["count_limbs_m2"]))&&
@@ -9,40 +19,47 @@
     {
         if($_POST["check"])
         {
-            $Insert_form=$conn->prepare("INSERT INTO Ex3 (Fio, Email, Date_birth, Sex, Count_limbs, Abilitys, Biography) 
-            VALUES (:Fio, :Email, :Date_birth, :Sex, :Count_limbs, :Abilitys, :Biography)");
+            $user='u47542';
+            $pass='7615565';
+            $conn= new PDO('mysql:host=localhost;dbname=u47542', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+            try {
+                $Insert_form=$conn->prepare("INSERT INTO Ex3 (Fio, Email, Date_birth, Sex, Count_limbs, Abilitys, Biography) 
+                VALUES (:Fio, :Email, :Date_birth, :Sex, :Count_limbs, :Abilitys, :Biography)");
 
-            $Insert_form->bindParam(':Fio',$Name);
-            $Insert_form->bindParam(':Email',$Email);
-            $Insert_form->bindParam(':Date_birth',$DateBirth);
-            $Insert_form->bindParam(':Sex',$Sex);
-            $Insert_form->bindParam(':Count_limbs',$Count_limbs);
-            $Insert_form->bindParam(':Abilitys',$Abilitys);
-            $Insert_form->bindParam(':Biography',$Biography);
+                $Insert_form->bindParam(':Fio',$Name);
+                $Insert_form->bindParam(':Email',$Email);
+                $Insert_form->bindParam(':Date_birth',$DateBirth);
+                $Insert_form->bindParam(':Sex',$Sex);
+                $Insert_form->bindParam(':Count_limbs',$Count_limbs);
+                $Insert_form->bindParam(':Abilitys',$Abilitys);
+                $Insert_form->bindParam(':Biography',$Biography);
 
-            $Name=$_POST["name"];
-            $Email=$_POST["email"];
-            $DateBirth=$_POST["date"];
-            $Sex=$_POST["sex"];
-            $count_limbs='>2';
-            $Count_limbs=$_POST["count_limbs"];
-            $Abilitys="";
-            $Superpowers=$_POST["superpowers"];
-            foreach($Superpowers as $power)
-                $Abilitys+=$power+", ";
-            $Abilitys-=", ";
-            $Biography=$_POST["biography"];
-
-            $Insert_pform=$Insert_form->execute();
-
+                $Name=$_POST["name"];
+                $Email=$_POST["email"];
+                $DateBirth=$_POST["date"];
+                $Sex=$_POST["sex"];
+                $count_limbs='>2';
+                $Count_limbs=$_POST["count_limbs"];
+                $Abilitys="";
+                $Superpowers=$_POST["superpowers"];
+                foreach($Superpowers as $power)
+                    $Abilitys+=$power+", ";
+                $Abilitys-=", ";
+                $Biography=$_POST["biography"];
+                $Insert_form->execute();
+            }catch(PDOException $e){
+                print('Error : ' . $e->getMessage());
+                exit();
+            }
         }
         else
-            print_r("Поставьте галочку!");
+            print("Поставьте галочку!");
     }
     else
     {   
-        print_r("Заполните все поля!");
+        print("Заполните все поля!");
     }
 
     header("Location: index3.html");
 ?>
+
