e5424bd1296cda6b54e1054e6aac986d56ba5ce8
HolyHotpants <91026415+HolyHotpants@users.noreply.github.com>
2022-03-28 20:04:18 +0300

    Update form.php

--- Задание 3/form.php
+++ Задание 3/form.php
@@ -10,15 +10,15 @@
         if($_POST["check"])
         {
             $Insert_form=$conn->prepare("INSERT INTO Ex3 (Fio, Email, Date_birth, Sex, Count_limbs, Abilitys, Biography) 
-            VALUES (:name, :email, :date, :sex, :count_limbs, :superpowers, :biography)");
+            VALUES (:Fio, :Email, :Date_birth, :Sex, :Count_limbs, :Abilitys, :Biography)");
 
-            $Insert_form->bindParam(':name',$Name);
-            $Insert_form->bindParam(':email',$Email);
-            $Insert_form->bindParam(':date',$DateBirth);
-            $Insert_form->bindParam(':sex',$Sex);
-            $Insert_form->bindParam(':count_limbs',$Count_limbs);
-            $Insert_form->bindParam(':superpowers',$Abilitys);
-            $Insert_form->bindParam(':biography',$Biography);
+            $Insert_form->bindParam(':Fio',$Name);
+            $Insert_form->bindParam(':Email',$Email);
+            $Insert_form->bindParam(':Date_birth',$DateBirth);
+            $Insert_form->bindParam(':Sex',$Sex);
+            $Insert_form->bindParam(':Count_limbs',$Count_limbs);
+            $Insert_form->bindParam(':Abilitys',$Abilitys);
+            $Insert_form->bindParam(':Biography',$Biography);
 
             $Name=$_POST["name"];
             $Email=$_POST["email"];
